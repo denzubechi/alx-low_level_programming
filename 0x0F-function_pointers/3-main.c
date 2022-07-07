5517535a9092d50ebf883257a03cc9ac3094b693
Samuel Nzubechi Chukwuma <98186867+denzubechi@users.noreply.github.com>
2022-07-06 19:47:00 -0700

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,17 +1,17 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - main file
- * @argc: number of lines arguments
- * @argv: array of elements
- * Return: 0
- */
-
+  * main - main.
+  * @argc: argument count.
+  * @argv: argument values.
+  * Return: 0.
+  */
 int main(int argc, char *argv[])
 {
-	int R;
+	int num1, num2, result;
+	int (*skrt)(int, int);
 
 	if (argc != 4)
 	{
@@ -19,8 +19,17 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	R = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
-	printf("%d\n", R);
+	num1 = atoi(argv[1]),
+	num2 = atoi(argv[3]);
+
+	skrt = get_op_func(argv[2]);
+	if (skrt == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = skrt(num1, num2);
+	printf("%d\n", result);
 
 	return (0);
 }
