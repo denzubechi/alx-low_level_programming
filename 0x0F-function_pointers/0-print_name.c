a70aae655889f9630868df7933fab587572e1ca6
Samuel Nzubechi Chukwuma <98186867+denzubechi@users.noreply.github.com>
2022-07-06 19:26:44 -0700

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,15 @@
 #include "function_pointers.h"
+
 /**
-  * print_name - prints a name
-  * @name: input string
-  * @f: function pointer casted to char pointer
-  */
+ * print_name - prints a name
+ * @name: name's main
+ * @f: void function
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (name && f)
+	{
+		f(name);
+	}
 }
