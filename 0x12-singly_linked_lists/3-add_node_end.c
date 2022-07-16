9a8d1d8f3e4d61f7998aa6bb85f5488432126c5b
Samuel Nzubechi Chukwuma <98186867+denzubechi@users.noreply.github.com>
2022-07-15 17:19:53 -0700

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,65 +1,70 @@
 #include "lists.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 /**
- * _strlen - a function that returns the length of a string.
- * @str: input string
- * Return: length of string
- */
-int _strlen(const char *str)
+  * _strlen - gets length of the string
+  * @s: string
+  * Return: length of the string
+  */
+int _strlen(const char *s)
 {
-int contador = 0;
+	int i;
 
-	while (str[contador] != '\0')
-	{
-		contador++;
-	}
-return (contador);
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
 }
 /**
- * add_node_end - print len.
- * @head: list.
- * @str: string.
- * Return: The elements linked.
- */
+  * _strdup - recreation of string duplicate function
+  * @src: source of string to duplicate
+  * Return: pointer to malloc'd space with copied string
+  */
+void *_strdup(const char *src)
+{
+	int len, i;
+	char *dest;
+
+	len = _strlen(src);
+	dest = malloc((len + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+  * add_node_end - add new nodes to the end of the list
+  * @head: current place in the list
+  * @str: string to add to the head
+  * Return: pointer to current position in list
+  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newnode;
-	list_t *endnode;
+	list_t *new, *current;
+	char *dupstr;
 
-	newnode = malloc(sizeof(list_t));
-	if (newnode == NULL)
-	{
-		free(newnode);
+	if (str == NULL)
 		return (NULL);
-	}
+	dupstr = _strdup(str);
+	if (dupstr == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = dupstr;
+	new->len = _strlen(str);
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = newnode;
+		*head = new;
+		return (*head);
 	}
-	else
-	{
-		endnode = *head;
-		while (endnode->next != NULL)
-			endnode = endnode->next;
-		endnode->next = newnode;
-	}
-
-	if (str == NULL)
-	{
-		newnode->str = 0;
-		newnode->len = 0;
-	}
-	else
-	{
-		newnode->str = strdup(str);
-		if (newnode->str == 0)
-		{
-			free(newnode);
-			return (NULL);
-		}
-		newnode->len = _strlen(str);
-	}
-	newnode->next = NULL;
-	return (newnode);
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = new;
+	return (*head);
 }
