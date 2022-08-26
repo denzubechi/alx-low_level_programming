7f2be1749292814138b3437ae20a1cb4671f36d8
denzubechi <Chukwumasamuel371@gmail.com>
2022-08-25 21:28:11 -0700

    bit manipulation'

---
+++ bash_script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "iAuthomated file used for testinv environmental variables when added to path, you could run this file by calling rhe file ne from any directory. how cool is that ?? :)"
